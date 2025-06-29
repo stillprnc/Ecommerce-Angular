14426d3b1c060412d8698f52d2699364cf7785e2
Prince Kumar <princ9861@gmail.com>
2025-06-30 01:48:13 +0530

    ui for order status

--- src/app/Module/feature/Component/order-details/order-details.component.ts
+++ src/app/Module/feature/Component/order-details/order-details.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { AddressCardComponent } from '../../../shared/Component/address-card/address-card.component';
+import { OrderCardComponent } from '../order/order-card/order-card.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-order-details',
   standalone: true,
-  imports: [],
+  imports: [AddressCardComponent, OrderCardComponent, CommonModule],
   templateUrl: './order-details.component.html',
   styleUrl: './order-details.component.css'
 })
 export class OrderDetailsComponent {
-
+  Orders=[1,1,1]
 }
